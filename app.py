87100f3ac5540c1e67d49625f862d60e38ae187d
Rishit Mahapatra <rishit_mahapatra@yahoo.com>
2025-05-30 01:49:46 +0530

    4th commit
    includes the new job page for respective jobs and created a new nav bar based on the old design for jobs page with new scroll-spies

--- app.py
+++ app.py
@@ -2,21 +2,10 @@ from flask import Flask , render_template
 from flask import jsonify
 from database import engine #importing the engine connection from the database.py Note that other files can be also treated as modules
 from sqlalchemy import text 
-
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
-
-
-def load_jobs_from_db():
-    with engine.connect() as conn:
-    
-        result = conn.execute(text("select * from jobs"))
-    
-        jobs = []
-        for row in result.all():
-            jobs.append(dict(row._mapping))
-    return jobs       #this function would return the list of jobs 
 
 
 
@@ -27,10 +16,18 @@ def function():
     jobs = load_jobs_from_db() #this would call the function to get the list of jobs from the db 
     return  render_template('home.html', jobs = jobs) #name used in home.html = name used in getting the list 
 
-@app.route("/jobs")
+@app.route("/jobs")  
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+
+@app.route("/job/<id>") #for single job page 
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job: #if the job list is empty 
+        return "Page Not Found", 404 
+    return render_template("job_page.html", job = job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -14,6 +14,25 @@ engine = create_engine(DATABASE_URL)
 
 
 
+def load_jobs_from_db():   #loading all the jobs in the apply section 
+    with engine.connect() as conn:
+    
+        result = conn.execute(text("select * from jobs"))
+    
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row._mapping))
+    return jobs       #this function would return the list of jobs 
 
+
+def load_job_from_db(id):   #loading the single job for the job page to be displayed 
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs where id = :val"), 
+                             {"val": id})
+        rows = result.all()
+        if len(rows)== 0 : #does not exist 
+            return None 
+        else: 
+            return dict(rows[0]._mapping) #important to put mapping 
 
     
